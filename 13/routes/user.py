d1532f90736b682501a61823eeba2e1c4b8c6eb5
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-09-13 07:00:53 -0300

    Update user.py

--- 13/routes/user.py
+++ 13/routes/user.py
@@ -4,8 +4,27 @@ user_route = Blueprint('user', __name__)
 
 @user_route.route('/')
 def lista_usuarios():
-
+    pass
+@user_route.route('/', methods=["POST"])
+def inserir_usuarios():
+    pass
 @user_route.route('/<str:user_id>')
-def obter_usuarios(user_id):
+def inserir_usuarios(user_id):
+    pass
 
-    
+'''
+
+
+
+
+/users/ (GET) -> lista de usuaruios
+/users/ (POST) -> inserir usuario no server
+/users/new (GET) -> criar um usuario
+/users/<id> (GET) -> mostrar o perfil do usuario
+/users/<id>/edit (GET) -> mostrar um formulario para alterar o perfil
+/users/<id>/atualizar (PUT) -> atualiza os dados do cliente
+/users/<id>/delete (DELETE) -> deleta um perfil
+
+
+
+'''
