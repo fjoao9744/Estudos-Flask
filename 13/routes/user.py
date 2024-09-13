e0f14bdb8c5d0eb41f0f28540c046956e1c8f635
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-09-13 20:06:58 -0300

    update 13

--- 13/routes/user.py
+++ 13/routes/user.py
@@ -4,17 +4,33 @@ user_route = Blueprint('user', __name__)
 
 @user_route.route('/')
 def lista_usuarios():
-    pass
-@user_route.route('/', methods=["POST"])
+    return "lista de usuarios"
+@user_route.route('/', methods=["POST"]) #inserir os dados do cliente no banco de dados
 def inserir_usuarios():
     pass
-@user_route.route('/<str:user_id>')
-def inserir_usuarios(user_id):
+@user_route.route('/new') #formulario para criar um user
+def form_cliente():
+    return {"pagina":"formulario de usuarios"}
+
+@user_route.route('/<int:user_id>') #pegando o id de um usuario
+def user(user_id):
+    #exibir detalhes do cleinte
     pass
 
+@user_route.route('/<int:user_id>/edit') #formulario para editar um perfil de usuario
+def edit_user(user_id):
+    pass
+
+@user_route.route('/<int:user_id>/update', methods=["PUT"]) #atualizar o perfil do usuario
+def up_user(user_id):
+
+    pass
+
+@user_route.route('/<int:user_id>/delete', methods=["DELETE"]) #deletando um usuario
+def del_user(user_id):
+
+    pass
 '''
-
-
 
 
 /users/ (GET) -> lista de usuaruios
